ebc8e7e434ac63460362a9b1d03d05791829f1d7
ShubhamPB <shubhamborase467@gmail.com>
2024-11-26 22:11:11 +0530

    Lombok added

--- src/main/java/com/sap/Conversession/modal/UserModal.java
+++ src/main/java/com/sap/Conversession/modal/UserModal.java
@@ -2,36 +2,17 @@ package com.sap.Conversession.modal;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserModal {
 	@Id
 	private String email;
 	private String username;
 	private String password;
-	
-	public UserModal() {
-	
-		
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
 }
