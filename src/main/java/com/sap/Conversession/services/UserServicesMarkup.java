0ab36df5ef316583d970262270972b312faf1add
ShubhamPB <shubhamborase467@gmail.com>
2024-11-27 00:30:36 +0530

    Demo Login module created after Register module

--- src/main/java/com/sap/Conversession/services/UserServicesMarkup.java
+++ src/main/java/com/sap/Conversession/services/UserServicesMarkup.java
@@ -4,4 +4,5 @@ import com.sap.Conversession.modal.UserModal;
 
 public interface UserServicesMarkup {
 	boolean register(UserModal u) ;
+	boolean validateUser(String email, String username, String password);
 }
